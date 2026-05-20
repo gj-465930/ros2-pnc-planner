6bbd2f9cf9d0fc093baea8686854a389dc614ee7
gj-465930 <2962901891@qq.com>
2026-05-20 11:53:06 +0800

    feat:实现笛卡尔与frenet坐标系双向转换模块

---
+++ src/pnc_planner/include/pnc_planner/math_utils/cartesian_frenet.hpp
@@ -0,0 +1,49 @@
+#pragma once
+
+#include <functional>
+
+namespace pnc_planner {
+namespace math_utils {
+
+/**
+ * @brief 实现笛卡尔坐标系与frenet坐标系的转换
+ */
+class CartesianFrenetConverter {
+  CartesianFrenetConverter() = delete;
+
+  using EvaluateCurveFunc =
+      std::function<void(double s, double &x, double &y, double &heading)>;
+
+  /**
+   * @brief 笛卡尔坐标系转换frenet坐标系
+   * @param target_x 目标点笛卡尔坐标 X
+   * @param target_y 目标点笛卡尔坐标 Y
+   * @param max_s 曲线最大长度
+   * @param eval_func 曲线求值回调函数
+   * @param s [out] 算出的纵向里程
+   * @param l [out] 算出的横向偏差
+   * @return true 转换成功; false 转换失败
+   */
+  static bool cartesianToFrenet(const double target_x, const double target_y,
+                                const double max_s,
+                                const EvaluateCurveFunc &eval_func, double &s,
+                                double &l);
+
+  /**
+   * @brief Frenet 坐标转笛卡尔坐标 (SL -> XY)
+   * @param s 目标点的纵向里程
+   * @param l 目标点的横向偏差
+   * @param max_s 曲线的最大长度
+   * @param eval_func 曲线求值回调函数 (闭包)
+   * @param x [out] 算出的世界坐标 X
+   * @param y [out] 算出的世界坐标 Y
+   * @return true 转换成功; false 越界等异常
+   */
+  static bool frenetToCartesian(const double s, const double l,
+                                const double max_s,
+                                const EvaluateCurveFunc &eval_func, double &x,
+                                double &y);
+};
+
+} // namespace math_utils
+} // namespace pnc_planner
---
+++ src/pnc_planner/src/math_utils/cartesian_frenet.cpp
@@ -0,0 +1,95 @@
+#include "pnc_planner/math_utils/cartesian_frenet.hpp"
+
+#include <algorithm>
+#include <cmath>
+#include <limits>
+#include <vector>
+
+namespace pnc_planner {
+namespace math_utils {
+
+bool CartesianFrenetConverter::cartesianToFrenet(
+    const double target_x, const double target_y, const double max_s,
+    const EvaluateCurveFunc &eval_func, double &s, double &l) {
+  if (max_s < 0.0 || !eval_func)
+    return false;
+
+  // 找到垂足并且计算最目标点与垂足的距离
+
+  double best_s = 0.0; // 当前找到的最近点的s值
+  double min_dist_sq = std::numeric_limits<double>::max();
+  // 步长
+  std::vector<double> steps = {1.0, 0.1, 0.01};
+
+  double serach_start = 0.0;
+  double serach_end = max_s;
+
+  double temp_x = 0.0, temp_y = 0.0, temp_heading = 0.0;
+
+  for (double step : steps) {
+    double curr_best_s = best_s;
+    for (double curr_s = serach_start; curr_s <= serach_end; curr_s += step) {
+      eval_func(curr_s, temp_x, temp_y, temp_heading);
+
+      double dx = temp_x - target_x;
+      double dy = temp_y - target_y;
+      double dist_sq = dx * dx + dy * dy;
+
+      if (dist_sq < min_dist_sq) {
+        min_dist_sq = dist_sq;
+        curr_best_s = curr_s;
+      }
+    }
+
+    best_s = curr_best_s;
+    serach_start = std::max(0.0, best_s - 2 * step);
+    serach_end = std::min(max_s, best_s + 2 * step);
+  }
+
+  s = best_s;
+
+  // 判断l的正负（根据叉乘出来z的方向向量判断）
+
+  eval_func(s, temp_x, temp_y, temp_heading);
+
+  // 切向量
+  double tau_x = std::cos(temp_heading);
+  double tau_y = std::sin(temp_heading);
+
+  double dx = target_x - temp_x;
+  double dy = target_y - temp_y;
+
+  double distance = std::sqrt(min_dist_sq);
+
+  double cross_product = tau_x * dy - tau_y * dx;
+
+  l = (cross_product > 0) ? distance : -distance;
+  return true;
+}
+
+bool CartesianFrenetConverter::frenetToCartesian(
+    const double s, const double l, const double max_s,
+    const EvaluateCurveFunc &eval_func, double &x, double &y) {
+  if (s < 0 || s > max_s || !eval_func) {
+    return false;
+  }
+
+  double ref_x = 0.0, ref_y = 0.0, ref_heading = 0.0;
+  eval_func(s, ref_x, ref_y, ref_heading);
+
+  // 切向量
+  double tau_x = std::cos(ref_heading);
+  double tau_y = std::sin(ref_heading);
+
+  // 法向量
+  double nor_x = -tau_y;
+  double nor_y = tau_x;
+
+  x = ref_x + l * nor_x;
+  y = ref_y + l * nor_y;
+
+  return true;
+}
+
+} // namespace math_utils
+} // namespace pnc_planner
